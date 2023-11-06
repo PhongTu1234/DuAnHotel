6322e7a15147ab2a8c1d125ef1c8909e6d94300d
HOANGVVM\Admin <hoangvvm@imt-soft.com>
2023-11-06 16:02:32 +0700

    Featured_Hotel

--- src/main/java/com/poly/controller/HomeController.java
+++ src/main/java/com/poly/controller/HomeController.java
@@ -64,6 +64,9 @@ public class HomeController {
 		int countTop4 = HotelTop4.size();
 		model.addAttribute("countTop4", countTop4);
 
+		
+		List<Hotels> HotelLevel5 = htservice.findHotelByHotelLevel5();
+		model.addAttribute("HotelLevel5", HotelLevel5);
 		return "index";
 	}
 	
